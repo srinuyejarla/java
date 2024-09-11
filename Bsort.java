e6a9b68511fc780e691a16629e9fe849d346c15d
srinuyejarla <srinuyejarla123@gmail.com>
2024-09-11 10:42:15 +0530

    Add files via upload

---
+++ Bsort.java
@@ -0,0 +1,51 @@
+public class BubbleSort {
+    // Method to perform bubble sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        
+        // Traverse through all elements in the array
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            
+            // Last i elements are already in place
+            for (int j = 0; j < n - i - 1; j++) {
+                // Compare adjacent elements
+                if (arr[j] > arr[j + 1]) {
+                    // Swap if they are in the wrong order
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    // Method to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method to test the bubble sort
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        bubbleSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+}
+
